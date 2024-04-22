c0e7e2df1bd713a1595a51fd3b12b29298f47413
bek123456789 <redmirating@gmail.com>
2024-04-22 21:50:56 +0500

    done

---
+++ main.js
@@ -0,0 +1,26 @@
+let producPrompt = +prompt("7 gacha son kiriting! ");
+
+switch (producPrompt) {
+  case 1:
+    alert("Dushanba");
+    break;
+  case 2:
+    alert("Seshanba");
+    break;
+  case 3:
+    alert("Chorshanba");
+    break;
+  case 4:
+    alert("Payshanba");
+    break;
+  case 5:
+    alert("Juma");
+    break;
+  case 6:
+    alert("Shanba");
+    break;
+  case 7:
+    alert("Yakshanba");
+  default:
+    console.log("1 haftada 7 kun bor!");
+}
